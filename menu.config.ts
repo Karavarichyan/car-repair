9b7cf56e14e11104450c1f89bbb34f52f4609526
Narek <narekkaravarichyan1910@gmail.com>
2025-03-15 17:22:58 +0400

    first commit

---
+++ menu.config.ts
@@ -0,0 +1,12 @@
+// Define the menu items
+export const mainMenu = {
+  home: "/",
+  services: "/services",
+  about: "/about",
+};
+
+export const contentMenu = {
+  categories: "/posts/categories",
+  tags: "/posts/tags",
+  authors: "/posts/authors",
+};
---
+++ site.config.ts
@@ -0,0 +1,22 @@
+// type SiteConfig = {
+//   site_domain: string;
+//   site_name: string;
+//   site_description: string;
+// };
+
+// export const siteConfig: SiteConfig = {
+//   site_name: "next-wp",
+//   site_description: "Starter template for Headless WordPress with Next.js",
+//   site_domain: "https://next-wp.com",
+// };
+type SiteConfig = {
+  site_domain: string;
+  site_name: string;
+  site_description: string;
+};
+
+export const siteConfig: SiteConfig = {
+  site_name: "մեքենաների վերանորոգում",
+  site_description: " մեքենաների վերանորոգմանը",
+  site_domain: "https://car-repair.am",
+};
